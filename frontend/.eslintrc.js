a31ff825a6e5282c32b87f1b997d142806b45c28
Ameer Ameen <ameerapex496@gmail.com>
2024-02-05 00:07:09 +0100

    trying to fix eslint erorrs

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,23 +4,36 @@ module.exports = {
     es2021: true,
   },
   extends: 'airbnb',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
+    'linebreak-style': 'off',
+    semi: 'off',
   },
-};
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'], // Adjust the patterns to match your project files
+      rules: {
+        'linebreak-style': 'off',
+        semi: 'off',
+      },
+    },
+    {
+      files: [
+        '.eslintrc.js',
+        '.eslintrc.cjs',
+        '.eslintrc.xjs',
+        '.eslintrc.ts',
+        '.eslintrc.tsx',
+      ],
+      env: {
+        node: true,
+      },
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+}
