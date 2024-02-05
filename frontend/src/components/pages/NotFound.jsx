4def635c44ef9bf682d78bf8fa3206ff2a6374dd
Ameer Ameen <ameerapex496@gmail.com>
2024-02-05 04:13:03 +0100

    small change in navbar + notfound page is styled even more better

--- frontend/src/components/pages/NotFound.jsx
+++ frontend/src/components/pages/NotFound.jsx
@@ -5,7 +5,7 @@ import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 const NotFound = () => {
   return (
-    <div className="h-[93dvh]  flex flex-col text-center items-center justify-center xs:text-[1.2rem] sm:text-[2rem] md:text-[3rem] lg:text-[4.5rem] xl:text-[5.5rem] 2xl:text-[6.7rem] bg-slate-700 text-white">
+    <div className="h-[100dvh]  flex flex-col text-center items-center justify-center xs:text-[1.2rem] sm:text-[2rem] md:text-[3rem] lg:text-[4.5rem] xl:text-[5.5rem] 2xl:text-[6.7rem] bg-slate-700 text-white">
       <div className="flex flex-col text-center justify-center items-center ">
         <h1>404</h1>
         <FontAwesomeIcon icon={faTriangleExclamation} />
