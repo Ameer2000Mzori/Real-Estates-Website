8e3360077496d550fd467ccb93f03048a4fb8f26
Ameer Ameen <ameerapex496@gmail.com>
2024-02-05 04:10:18 +0100

    added awesomefont and styled the not found page

--- frontend/src/components/pages/NotFound.jsx
+++ frontend/src/components/pages/NotFound.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable import/prefer-default-export, arrow-body-style */
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 const NotFound = () => {
   return (
-    <div className="h-[90vh]  flex flex-col text-center items-center justify-center">
-      <h1 className="xs:text-[1.2rem] sm:text-[2rem] md:text-[3rem] lg:text-[4.5rem] xl:text-[5.5rem] 2xl:text-[6.7rem]">
-        404
-      </h1>
-      <h2 className="xs:text-[1.2rem] sm:text-[2rem] md:text-[3rem] lg:text-[4.5rem] xl:text-[5.5rem] 2xl:text-[6.7rem]">
-        Page Not Found ˙◠˙
-      </h2>
+    <div className="h-[93dvh]  flex flex-col text-center items-center justify-center xs:text-[1.2rem] sm:text-[2rem] md:text-[3rem] lg:text-[4.5rem] xl:text-[5.5rem] 2xl:text-[6.7rem] bg-slate-700 text-white">
+      <div className="flex flex-col text-center justify-center items-center ">
+        <h1>404</h1>
+        <FontAwesomeIcon icon={faTriangleExclamation} />
+      </div>
+      <h2>Page Not Found</h2>
     </div>
   )
 }
