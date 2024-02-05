d3f88bd0d5e7a1092a3def88bdfb40bcad10481b
Ameer Ameen <ameerapex496@gmail.com>
2024-02-05 02:16:58 +0100

    removed image and changed website name and title

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,13 +14,8 @@ function Navbar() {
           href="https://github.com/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            real estate
           </span>
         </a>
         <button
