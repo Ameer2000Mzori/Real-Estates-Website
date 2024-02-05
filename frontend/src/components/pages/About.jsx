3fcc99f58b2667f7d61f1c3a8d73304a2fe437a1
Ameer Ameen <ameerapex496@gmail.com>
2024-02-05 01:25:53 +0100

    added new pages in pages folder new files called HomePage and About

---
+++ frontend/src/components/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <>
+      <h1>ABOUT PAGE</h1>
+      <h1>ABOUT PAGE</h1>
+    </>
+  )
+}
---
+++ frontend/src/components/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HomePage = () => {
+  return (
+    <>
+      <h1>HOME PAGE</h1>
+      <h1>HOME PAGE</h1>
+    </>
+  )
+}
