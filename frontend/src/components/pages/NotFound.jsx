0aab903b6df240a098c7273147f8bb2a4fee29be
Ameer Ameen <ameerapex496@gmail.com>
2024-02-05 03:19:20 +0100

    added styling for div wrap text

--- frontend/src/components/pages/NotFound.jsx
+++ frontend/src/components/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="h-[90vh]  flex flex-col text-center items-center justify-center">
       <h1>404</h1>
       <h2>Page Not Found</h2>
     </div>
