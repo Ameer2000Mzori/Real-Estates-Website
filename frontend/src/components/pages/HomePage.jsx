e56eb377f1df18f0b3d07243ab99086fea907258
Ameer Ameen <ameerapex496@gmail.com>
2024-02-05 05:06:11 +0100

    added styling to div wrap and ul

--- frontend/src/components/pages/HomePage.jsx
+++ frontend/src/components/pages/HomePage.jsx
@@ -5,13 +5,15 @@ const HomePage = () => {
   // eslint-disable-next-line
 
   return (
-    <ul>
-      {housesObjs.map((house) => (
-        <li key={house.id}>
-          <p>${house.price}</p>
-        </li>
-      ))}
-    </ul>
+    <div className="flex flex-row  h-[100dvh]  ">
+      <ul className=" flex flex-row text-center items-center justify-center gap-4 w-[100%] h-[100%]">
+        {housesObjs.map((house) => (
+          <li key={house.id}>
+            <p>${house.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 export default HomePage
