08709c1d7a1ce095205cf48ef47f1e54583024a6
Ameer Ameen <ameerapex496@gmail.com>
2024-02-05 05:15:15 +0100

    small styling in home page

--- frontend/src/components/pages/HomePage.jsx
+++ frontend/src/components/pages/HomePage.jsx
@@ -5,14 +5,17 @@ const HomePage = () => {
   // eslint-disable-next-line
 
   return (
-    <div className="flex flex-row  h-[100dvh] text-center items-center justify-center ">
-      <ul className=" flex flex-row text-center items-center justify-center gap-4 w-[80%] h-[100%] flex-wrap">
+    <div className="flex flex-row  h-[100dvh] text-center items-center justify-center bg-slate-600">
+      <ul className=" flex flex-row text-center items-center justify-center gap-4 w-[40%] h-[600px] flex-wrap">
         {housesObjs.map((house) => (
-          <li className="w-[350px] h-[250px]" key={house.id}>
+          <li className="w-[250px] h-[250px] bg-white" key={house.id}>
             <p>${house.price}</p>
           </li>
         ))}
       </ul>
+      <div>
+        <li className="w-[650px] h-[500px] bg-white">ss</li>
+      </div>
     </div>
   )
 }
