ab011ae6a63abef5f8547f9fd784f1079d2db8d0
Ameer Ameen <ameerapex496@gmail.com>
2024-02-05 03:32:23 +0100

    added styling to both texts and made them responsive

--- frontend/src/components/pages/NotFound.jsx
+++ frontend/src/components/pages/NotFound.jsx
@@ -4,8 +4,12 @@ import React from 'react'
 const NotFound = () => {
   return (
     <div className="h-[90vh]  flex flex-col text-center items-center justify-center">
-      <h1>404</h1>
-      <h2>Page Not Found</h2>
+      <h1 className="xs:text-[1.2rem] sm:text-[2rem] md:text-[3rem] lg:text-[4.5rem] xl:text-[5.5rem] 2xl:text-[6.7rem]">
+        404
+      </h1>
+      <h2 className="xs:text-[1.2rem] sm:text-[2rem] md:text-[3rem] lg:text-[4.5rem] xl:text-[5.5rem] 2xl:text-[6.7rem]">
+        Page Not Found ˙◠˙
+      </h2>
     </div>
   )
 }
