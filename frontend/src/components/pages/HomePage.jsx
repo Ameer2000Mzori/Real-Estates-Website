8d1bc3497098b903aea31fbeee2019c6b312658d
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-09 04:03:56 +0100

    added more styling to the cards

--- frontend/src/components/pages/HomePage.jsx
+++ frontend/src/components/pages/HomePage.jsx
@@ -6,16 +6,22 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-row  h-[100dvh] text-center items-center justify-center bg-slate-600">
-      <ul className="house-list flex flex-row text-center items-center justify-center gap-4 w-[40%] h-[600px] flex-wrap">
+      <ul className="house-list flex flex-row text-center items-center justify-center gap-4 w-[60%] h-[600px] flex-wrap">
         {housesObjs.map((house) => (
-          <li className="w-[250px] h-[250px] bg-white" key={house.id}>
-            <p>${house.price}</p>
+          <li
+            className="w-[250px] h-[250px] bg-white flex flex-col text-center items-center justify-center"
+            key={house.id}
+          >
+            <div className="h-[80%] w-[100%] bg-white flex flex-col text-center items-center justify-center border-b-2 border-gray-300">
+              <h2 className="text-gray-400 text-md font-bold">250PX / 250PX</h2>
+            </div>
+            <div className="h-[20%] w-[100%] bg-slate-300 flex flex-col text-center items-center justify-evenly border-b-2 border-gray-300">
+              <p>{house.price}</p>
+              <button type="button">add to bookmarks</button>
+            </div>
           </li>
         ))}
       </ul>
-      <div>
-        <li className="w-[650px] h-[500px] bg-white">ss</li>
-      </div>
     </div>
   )
 }
