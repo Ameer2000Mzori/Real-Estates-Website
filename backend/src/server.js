aa24ba4a940a7e47e6c70fec4ebdc5a99ce8bc4c
Ameer Ameen <ameerapex496@gmail.com>
2024-02-11 02:17:54 +0100

    added mangodb to the project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
 import router from './route/routes.js'
+import mongoConnect from './db/db.js'
 
 const app = express()
 app.use(express.json())
@@ -8,6 +9,8 @@ app.use(router)
 // our port :
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, (req, res) => {
-  console.log(`server is running at port ${PORT}`)
+mongoConnect((cb) => {
+  app.listen(PORT, (req, res) => {
+    console.log(`server is running at port ${PORT}`)
+  })
 })
