c6387eb8ea8bc600fee18fca9ce1102725ff84b1
Ameer Ameen <ameerapex496@gmail.com>
2024-02-11 01:31:14 +0100

    added logc for - env and made a port

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,15 @@
-console.log('Hello World! from backend.')
+import express from 'express'
+import 'dotenv/config'
+
+const app = express()
+
+// our port :
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('this is home page')
+})
+
+app.listen(PORT, (req, res) => {
+  console.log(`server is running at port ${PORT}`)
+})
